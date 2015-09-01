e1243a27135476d534fb7bbc2dedcefcec71cdc3
Bill Carlson <bill@coacoas.net>
2015-08-31 23:04:27 -0400

    Same functionality, but using jQuery now

--- build.sbt
+++ build.sbt
@@ -4,4 +4,4 @@ scalaVersion := "2.11.7"
 scalaJSStage in Global := FastOptStage
 skip in packageJSDependencies := false
 
-libraryDependencies += "be.doeraene" %%% "scalajs-jquery" % "0.8.0"
+libraryDependencies +=0 "be.doeraene" %%% "scalajs-jquery" % "0.8.0"
--- src/main/scala/presentation/Hello.scala
+++ src/main/scala/presentation/Hello.scala
@@ -1,12 +1,14 @@
 package presentation
 
+import org.scalajs.jquery.jQuery
 import scala.scalajs.js.JSApp
 import scala.scalajs.js.annotation.JSExport
-import org.scalajs.jquery.jQuery
-
 
 object Hello extends JSApp {
-  def main() = {
+  @JSExport
+  def addClickedMessage() =
+    jQuery("body").append("<p>You clicked the button!</p>")
+
+  def main() = 
     jQuery("body").append("<p>Hello, JavaScript Dependencies!</p>")
-  }
 }
