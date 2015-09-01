bf69b10450966ee0fe6d1631f25bf14297dcf9ab
Bill Carlson <bill@coacoas.net>
2015-08-31 22:21:31 -0400

    Initial jQuery installation

--- build.sbt
+++ build.sbt
@@ -4,4 +4,4 @@ scalaVersion := "2.11.7"
 scalaJSStage in Global := FastOptStage
 skip in packageJSDependencies := false
 
-libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "0.8.0"
+libraryDependencies += "be.doeraene" %%% "scalajs-jquery" % "0.8.0"
--- src/main/scala/presentation/Hello.scala
+++ src/main/scala/presentation/Hello.scala
@@ -1,22 +1,12 @@
 package presentation
 
 import scala.scalajs.js.JSApp
-
-import org.scalajs.dom
-import dom.document
 import scala.scalajs.js.annotation.JSExport
+import org.scalajs.jquery.jQuery
+
 
 object Hello extends JSApp {
-  def appendPar(targetNode: dom.Node, text: String) = {
-    val parNode = document.createElement("p")
-    val textNode = document.createTextNode(text)
-    parNode.appendChild(textNode)
-    targetNode.appendChild(parNode)
+  def main() = {
+    jQuery("body").append("<p>Hello, jQuery!</p>")
   }
-
-  @JSExport
-  def addClickedMessage() =
-    appendPar(document.body, "You clicked the button!")
-
-  def main() = appendPar(document.body, "Hello, World!")
 }
