833ffa41c233be70d24298011c2a96b677200030
Bill Carlson <bill@coacoas.net>
2015-08-31 21:22:29 -0400

    Add DOM processing

--- build.sbt
+++ build.sbt
@@ -3,3 +3,5 @@ enablePlugins(ScalaJSPlugin)
 scalaVersion := "2.11.7"
 scalaJSStage in Global := FastOptStage
 skip in packageJSDependencies := false
+
+libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "0.8.0"
--- src/main/scala/presentation/Hello.scala
+++ src/main/scala/presentation/Hello.scala
@@ -2,8 +2,16 @@ package presentation
 
 import scala.scalajs.js.JSApp
 
+import org.scalajs.dom
+import dom.document
+
 object Hello extends JSApp {
-  def main(): Unit = {
-    println("Hello, world!")
+  def appendPar(targetNode: dom.Node, text: String) = {
+    val parNode = document.createElement("p")
+    val textNode = document.createTextNode(text)
+    parNode.appendChild(textNode)
+    targetNode.appendChild(parNode)
   }
+
+  def main() = appendPar(document.body, "Hello, World!")
 }
