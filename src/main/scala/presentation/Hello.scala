259ba7e597359ca048b3d6194fdcde57304603af
Bill Carlson <bcarlson@vorrohealth.com>
2015-09-30 09:39:14 -0400

    Counter button

--- src/main/scala/presentation/Hello.scala
+++ src/main/scala/presentation/Hello.scala
@@ -1,9 +1,21 @@
 package presentation
 
+import org.scalajs.dom._
+import org.scalajs.jquery.{jQuery => $}
+
 import scala.scalajs.js.JSApp
+import scala.scalajs.js.annotation.JSExport
 
 object Hello extends JSApp {
-  def main(): Unit = {
-    println("Hello, world!")
+  var clicked = 0
+
+  @JSExport
+  override def main(): Unit = {
+    $(document).ready {
+      $("#click-me").click { (ev: Event) =>
+        clicked += 1
+        $(ev.target).html(s"Clicked $clicked times")
+      }
+    }
   }
 }
